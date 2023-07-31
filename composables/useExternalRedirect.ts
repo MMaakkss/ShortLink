da8363488b0134ad839899ca6972c0460a2c7040
Maksmm <maks_minakov@icloud.com>
2023-07-31 22:08:23 +0300

    added functionality

---
+++ composables/useExternalRedirect.ts
@@ -0,0 +1,24 @@
+import { sendRedirect } from "h3";
+
+export default function useExternalRedirect(
+  url: string,
+  code: number = 302
+): Promise<void> {
+  if (!/^(https?:\/\/)/.test(url)) {
+    if (process.server) {
+      const nuxtApp = useNuxtApp();
+
+      if (nuxtApp.ssrContext?.event) {
+        return nuxtApp.callHook("app:redirected").then(() => {
+          if (nuxtApp.ssrContext?.event) {
+            return sendRedirect(nuxtApp.ssrContext.event, url, code);
+          }
+        });
+      }
+    } else {
+      window.location.href = url;
+    }
+  }
+
+  throw new Error("Invalid url");
+}
